00a3b84ca24c31cd73ba79db1799552a2d71c128
unknown <Pixel Electric@PIXEL-OPERATIONS.pixel.co.ke>
2024-04-26 08:58:55 +0300

    Upload 3d model

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,21 +12,9 @@ function App() {
         </div>
       </div>
 
-{/* //upload file section */}
       <div className="upload">
         <FileUpload />
       </div>
-
-      {/* view uploaded file */}
-      <div className="viewUpload">
-        <p>Viewing upload</p>
-        <form method='POST' action="/upload" enctype="multipart/form-data" >
-          <input type="file" name="image" />
-            <input type="submit" name="" id="" />
-
-        </form>
-      </div>
-
 
     </div>
   );
